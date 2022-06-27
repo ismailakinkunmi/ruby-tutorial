3ee1036b7843fe60dc99b751487d75670eed83b8
Adekunle Ismail <adismail4@gmail.com>
2022-06-27 23:29:15 +0100

    Modify ex21.rb

--- ex21.rb
+++ ex21.rb
@@ -20,3 +20,18 @@ end
 
 
 puts "Let's do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+
+# A puzzle for the extra credit, type it in anyway.
+puts "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+puts "That becomes: #{what}. Can you do it by hand?"
